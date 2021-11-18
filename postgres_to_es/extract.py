4bc2e1a48bf775cdef737682d78161192e65dd42
Maksim P <sl1ppery@mail.ru>
2021-11-18 19:10:45 +1000

    issue #1

--- postgres_to_es/extract.py
+++ postgres_to_es/extract.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Optional, Tuple
 from uuid import UUID
 
+import json
 import psycopg2
 from psycopg2 import OperationalError
 from psycopg2.extensions import connection
@@ -10,22 +11,7 @@ from psycopg2.extras import DictCursor
 
 from misc import backoff
 
-
-@dataclass
-class FilmWork:
-    id: UUID
-    title: str
-    type: str
-    created_at: datetime
-    updated_at: datetime
-    person_id: UUID
-    greatest: datetime
-    person_full_name: Optional[str] = None
-    genre: Optional[str] = None
-    person_role: Optional[str] = None
-    rating: Optional[float] = None
-    description: Optional[str] = None
-
+from models import FilmWorkIndex
 
 class Extract:
     """
@@ -60,30 +46,90 @@ class Extract:
         """
         with self.con.cursor() as cur:
             cur.execute("""
-                       SELECT
-                           fw.id as id, 
-                           fw.title as title, 
-                           fw.description as description, 
-                           fw.rating as rating, 
-                           fw.type as type, 
-                           fw.created_at as created_at, 
-                           fw.updated_at as updated_at, 
-                           pfw.role as person_role, 
-                           p.id as person_id, 
-                           p.full_name as person_full_name,
-                           g.name as genre,
-                           Greatest(fw.updated_at, p.updated_at, g.updated_at) as greatest
-                       FROM film_work fw
-                       LEFT JOIN person_film_work pfw ON pfw.film_work_id = fw.id 
-                       LEFT JOIN person p ON p.id = pfw.person_id
-                       LEFT JOIN genre_film_work gfw ON gfw.film_work_id = fw.id 
-                       LEFT JOIN genre g ON g.id = gfw.genre_id
-                       WHERE fw.updated_at > %(updated_at)s OR p.updated_at > %(updated_at)s OR g.updated_at > %(updated_at)s
-                       ORDER BY Greatest(fw.updated_at, p.updated_at, g.updated_at) ASC
-                       LIMIT %(limit)s;
-                   """, {'updated_at': last_update, 'limit': limit})
-            fetch_data = cur.fetchall()
-        data = [FilmWork(**dict(row)) for row in fetch_data]
-        if len(data) == 0:
-            return [], None
-        return data, data[-1].greatest
+                with
+                    constant_created_at AS (VALUES (cast(%s as timestamp with time zone))),
+                    constant_limit AS (VALUES (%s)),
+	                film_work_for_update as (
+	            select distinct
+		            film_work.id,
+		            max(least(film_work.created_at, genre_film_work.created_at, genre.created_at, person_film_work.created_at, person.created_at)) as updated_at
+	            from  
+		            "content".film_work as film_work
+			            left join "content".genre_film_work as genre_film_work
+				            left join "content".genre as genre
+					            on genre_film_work.genre_id = genre.id
+						            and coalesce(genre.created_at, '0001-01-01') > (table constant_created_at)
+				            on film_work.id = genre_film_work.film_work_id
+					            and coalesce(genre_film_work.created_at, '0001-01-01') > (table constant_created_at)
+			            left join "content".person_film_work as person_film_work
+				            left join "content".person as person
+					            on person_film_work.person_id = person.id
+						            and coalesce(person.created_at, '0001-01-01') > (table constant_created_at)
+				            on film_work.id = person_film_work.film_work_id
+					            and coalesce(person_film_work.created_at, '0001-01-01') > (table constant_created_at)
+	            where 
+		            least(film_work.created_at, genre_film_work.created_at, genre.created_at, person_film_work.created_at, person.created_at) > (table constant_created_at)
+	            group by
+		            film_work.id
+	            order by 
+		            max(least(film_work.created_at, genre_film_work.created_at, genre.created_at, person_film_work.created_at, person.created_at)),
+		            film_work.id
+	            limit (table constant_limit)),
+                data_to_export as(
+	                select 
+		                filmwork.id as uuid,
+		                filmwork.rating as imdb_rating,
+		                array_to_string(array_agg(distinct genre.name order by genre.name), ', ') as genres_names,
+		                jsonb_agg(distinct jsonb_strip_nulls(jsonb_build_object('uuid', genre.id, 'name', genre.name))) as genre,
+		                filmwork.title as title,
+		                filmwork.description as description,
+		                case when length(array_to_string(array_agg(distinct directors.full_name order by directors.full_name), ', ')) > 0 then array_to_string(array_agg(distinct directors.full_name order by directors.full_name), ', ')  else null end as director,
+		                case when length(array_to_string(array_agg(distinct actors.full_name order by actors.full_name), ', ')) > 0 then array_to_string(array_agg(distinct actors.full_name order by actors.full_name), ', ') else null end as actors_names,
+		                case when length(array_to_string(array_agg(distinct writers.full_name order by writers.full_name), ', ')) > 0 then array_to_string(array_agg(distinct writers.full_name order by writers.full_name), ', ') else null end as writers_names,
+		                jsonb_agg(distinct jsonb_strip_nulls(jsonb_build_object('uuid', directors.id, 'full_name', directors.full_name))) as directors,
+		                jsonb_agg(distinct jsonb_strip_nulls(jsonb_build_object('uuid', actors.id, 'full_name', actors.full_name))) as actors,
+		                jsonb_agg(distinct jsonb_strip_nulls(case when writers.id is null then null else jsonb_build_object('uuid', writers.id, 'full_name', writers.full_name) end)) as writers
+	                from 
+		                film_work_for_update as film_work_for_update
+			                inner join content.film_work as filmwork
+				                left join content.genre_film_work as genre_film_work
+					                left join content.genre as genre
+						                on genre_film_work.genre_id = genre.id
+					                on filmwork.id = genre_film_work.film_work_id
+				                left join content.person_film_work as directors_link
+					                left join content.person as directors
+						                on directors_link.person_id = directors.id
+					                on filmwork.id = directors_link.film_work_id 
+						                and directors_link.role = 'director'
+				                left join content.person_film_work as actors_link
+					                left join content.person as actors
+						                on actors_link.person_id = actors.id
+					                on filmwork.id = actors_link.film_work_id 
+						                and actors_link.role = 'actor'
+				                left join content.person_film_work as writers_link
+					                left join content.person as writers
+						                on writers_link.person_id = writers.id
+					                on filmwork.id = writers_link.film_work_id 
+						                and writers_link.role = 'writer'
+				                on film_work_for_update.id = filmwork.id 
+	                group by 
+		                filmwork.id,
+		                filmwork.rating,
+		                filmwork.title,
+		                filmwork.description,
+		                film_work_for_update.updated_at)
+                select 
+	                jsonb_agg(to_jsonb(data_to_export)),
+	                max(film_work_for_update.updated_at) as updated_at
+                from 
+	                data_to_export as data_to_export
+		                inner join film_work_for_update as film_work_for_update
+			                on data_to_export.uuid = film_work_for_update.id;
+                   """, (last_update, limit))
+            fetch_data = cur.fetchone()
+        
+        if fetch_data[0] is None:
+             return [], None
+
+        data = [FilmWorkIndex(**row) for row in fetch_data[0]]
+        return data, fetch_data[1]
--- postgres_to_es/transform.py
+++ postgres_to_es/transform.py
@@ -1,12 +1,15 @@
 import datetime
 import itertools
+import dataclasses
 import logging
-from typing import Optional
+from typing import List, Optional
 from uuid import UUID
 
 from pydantic import BaseModel
 
-from extract import FilmWork
+from models import FilmWorkIndex
+
+
 
 logger = logging.getLogger()
 
@@ -19,78 +22,31 @@ class ElPerson(BaseModel):
 class ElFilmWork(BaseModel):
     id: UUID
     imdb_rating: Optional[float] = None
-    genre: list[str]
+    genre: List[str]
     title: str
     description: Optional[str] = None
-    director: list[str]
-    actors_names: list[str]
-    writers_names: list[str]
-    actors: list[ElPerson]
-    writers: list[ElPerson]
+    director: List[str]
+    actors_names: List[str]
+    writers_names: List[str]
+    actors: List[ElPerson]
+    writers: List[ElPerson]
 
 
 class Transform:
     """
     The class implements transform data to load in Elasticsearch database
     """
-    def __init__(self, data: list[FilmWork]):
+    def __init__(self, data: List[FilmWorkIndex]):
         self.data = data
 
-    def _transform_data(self) -> list[ElFilmWork]:
-        """ This method transform data"""
-        last_date = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc)
-        for key, group in itertools.groupby(self.data, lambda x: x.id):
-            actors = []
-            writers = []
-            directors = []
-            genres = []
-            actors_names = []
-            writers_names = []
-            for fw_row in group:
-                if fw_row.person_id is not None:
-                    person = ElPerson(id=fw_row.person_id, name=fw_row.person_full_name)
-                    if fw_row.person_role == 'actor' and person not in actors:
-                        actors.append(person)
-                        actors_names.append(fw_row.person_full_name)
-                    if fw_row.person_role == 'writer' and person not in writers:
-                        writers.append(person)
-                        writers_names.append(fw_row.person_full_name)
-                    if fw_row.person_role == 'director' and fw_row.person_full_name not in directors:
-                        directors.append(fw_row.person_full_name)
-                if fw_row.genre not in genres:
-                    genres.append(fw_row.genre)
-                if fw_row.updated_at > last_date:
-                    last_date = fw_row.updated_at
-            cls = ElFilmWork(
-                id=fw_row.id,
-                imdb_rating=fw_row.rating,
-                genre=genres,
-                title=fw_row.title,
-                description=fw_row.description,
-                director=directors,
-                actors_names=actors_names,
-                writers_names=writers_names,
-                actors=actors,
-                writers=writers
-            )
-            logger.debug("Added FilmWork %s", (cls,))
-            yield cls
-
-    def get_data(self):
+    def get_data(self) -> List[dict]:
         """ The method returns a generator with prepared data """
-        for row in self._transform_data():
-            doc = {
+        actions = []
+        for item in self.data:
+            action = {
                 "_index": "movies",
-                "_id": str(row.id),
-                "id": str(row.id),
-                "imdb_rating": row.imdb_rating,
-                "genre": [", ".join(row.genre)],
-                "title": row.title,
-                "description": row.description,
-                "director": row.director if len(row.director) != 0 else None,
-                "actors_names": row.actors_names,
-                "writers_names": row.writers_names,
-                "actors": [i.dict() for i in row.actors],
-                "writers": [i.dict() for i in row.writers]
+                "_id": item.uuid,
+                "_source": dataclasses.asdict(item)
             }
-            yield doc
+            actions.append(action)
+        return actions
